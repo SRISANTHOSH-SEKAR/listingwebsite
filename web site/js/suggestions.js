da657138b660df48cb1729fec1ecf2aac3ab47cf
SRISANTHOSH SEKAR <83419129+ethicaleducater@users.noreply.github.com>
2023-02-09 21:44:56 +0530

    Add files via upload

---
+++ web site/js/suggestions.js
@@ -0,0 +1,18 @@
+let suggestions = [
+
+    "Hotel Deepam",
+    "Hotel Rockfort Veiw",
+    "Sri Maharaja Residency",
+    "Ramyas Hotel",
+    "Suraj Residency",
+    "Vivid a Boutique Hotel",
+    "Grade inn Trichy",
+    "Hotel Apple Park inn",
+    "KVM Hotels Srirangam",
+    "The scarlet Srirangam",
+    "Plaza Hotel Trichy",
+    "Grand Gardenia",
+    "Breeze Residency",
+    "Maya Residency"
+    
+];
